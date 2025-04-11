823d41dde6b1f471c2ee8f3c488b3e108fe1acbe
Brooklyn <44684333+BrooklynDipi@users.noreply.github.com>
2025-04-11 11:18:44 -0500

    Import testing assets

---
+++ Material2022/Assets/MaterialAssets/MaterialTestSceneScript.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialTestSceneScript : MonoBehaviour
+{
+    #region Dependencies
+
+    public static MaterialTestSceneScript Instance { get; private set; }
+
+    #endregion Dependencies
+
+    public Camera ScreenshotCamera;
+
+    public RawImage RawMinikinScreenshotImage;
+
+    public RawImage FinalImage;
+
+    public Material MinipicLayerBlenderMaterial;
+
+    public Texture2D BottomLayerTexture;
+
+    public Texture2D TopLayerTexture;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void TakeScreenshot()
+    {
+        RawMinikinScreenshotImage.texture = TakeMinikinScreenshot();
+    }
+
+    public void TakeMinipic()
+    {
+        var minikinScreenshot = TakeMinikinScreenshot();
+        RenderTexture textureOutput = new(minikinScreenshot.height, minikinScreenshot.width, 16);
+
+        MinipicLayerBlenderMaterial.SetTexture("_BG", BottomLayerTexture);
+        MinipicLayerBlenderMaterial.SetTexture("_RenderedMinikin", minikinScreenshot);
+        MinipicLayerBlenderMaterial.SetTexture("_FG", TopLayerTexture);
+
+        Graphics.Blit(null, textureOutput, MinipicLayerBlenderMaterial);
+
+        FinalImage.texture = textureOutput;
+    }
+
+    private Texture2D TakeMinikinScreenshot()
+    {
+        Texture2D rawMinikinScreenshot = new(ScreenshotCamera.activeTexture.width, ScreenshotCamera.activeTexture.height);
+        RenderTexture.active = ScreenshotCamera.activeTexture;
+
+        ScreenshotCamera.Render();
+
+        rawMinikinScreenshot.ReadPixels(new Rect(0, 0, ScreenshotCamera.activeTexture.width, ScreenshotCamera.activeTexture.height), 0, 0);
+        rawMinikinScreenshot.Apply();
+
+        RawMinikinScreenshotImage.texture = rawMinikinScreenshot;
+
+        return rawMinikinScreenshot;
+    }
+}
